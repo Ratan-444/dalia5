de0ec246894cae3063e4b6b537431f4f1b698569
Ratan-444 <ratanbadiger92@gmail.com>
2024-12-18 22:58:04 +0530

    added all files

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+import express from "express";
+import databaseConnection from "./utils/database.js";
+import cookieParser from "cookie-parser";
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
+
+databaseConnection();
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+};
+app.use(cors(corsOptions));
+
+// API routes
+app.use("/api/v1/user", userRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening at port ${process.env.PORT}`);
+});
