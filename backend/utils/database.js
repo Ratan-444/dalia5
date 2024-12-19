d7785818185606db34cf856884b64d7f6d4c7656
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2024-12-19 08:21:55 +0530

    Update database.js

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+// apiConfig.js
+export const API_END_POINT = "https://dalia5-lqro.vercel.app/api/v1/user";
 
-const databaseConnection = () => {
-    const mongoUri = process.env.MONGO_URI;
-    console.log("Connecting to:", mongoUri); // Debug log
-    mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("MongoDB connected successfully.");
-        })
-        .catch((error) => {
-            console.log("Error connecting to MongoDB:", error);
-        });
+export const options = {
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzN2FhYmU2MzRjOTNkMjk1OTRlOGJmNGExNTcyYTI4MCIsIm5iZiI6MTczNDUyNTUyNy42NzM5OTk4LCJzdWIiOiI2NzYyYzI1NzcxM2U5ZmY0NWJhYWZjYWYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.YKnuezia9IC2v54Qzv4ARyBLSal2P8jVLeKEFTDpIKU"
+  },
 };
 
-export default databaseConnection;
+// Movie API Endpoints
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query=";
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
